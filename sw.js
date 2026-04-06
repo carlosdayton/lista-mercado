25dfe1d5b4b41ea5d915c6321d4410957c7571d7
carlosdayton <daytonribeiro365@gmail.com>
2026-04-06 15:58:30 -0300

    fix(sw): limpar cache no client pois env da versao 3 estava vazio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lista-mercado-v3';
+const CACHE = 'lista-mercado-v4';
 const STATIC = ['/', '/index.html', '/style.css', '/dist/main.js', '/manifest.json', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
